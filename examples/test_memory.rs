63b2adb4288fdd18d77746a8614790f025ad4ace
ju6ge <judge@felixrichter.tech>
2020-05-09 14:28:53 +0200

    working memory setup + simple memory sanity test

--- examples/test_memory.rs
+++ examples/test_memory.rs
@@ -12,14 +12,17 @@ use atsamx7x_hal::gpio::*;
 use atsamx7x_hal::clock_gen::{Clocks, MasterClockConfig, SlckConfig, MainckConfig, PllackConfig, UpllckConfig, SystemClockConfig, MasterDivider, MasterPrescale};
 use atsamx7x_hal::serial::{config, Serial};
 use atsamx7x_hal::time::{MegaHertz, *};
+use atsamx7x_hal::delay::Delay;
+use embedded_hal::blocking::delay::{DelayMs};
 
 use core::fmt::Write;
+use target_device::SDRAMC;
 
 use board::mem::init_sdram;
 
 #[entry]
 fn main() -> ! {
-	let _cortex_p = cortex_m::Peripherals::take().unwrap();
+	let cortex_p = cortex_m::Peripherals::take().unwrap();
 	let peripherals = target_device::Peripherals::take().unwrap();
 
 	let wdt = &peripherals.WDT;
@@ -27,6 +30,11 @@ fn main() -> ! {
 
 	let mut pmc = peripherals.PMC;
 	let mut supc = peripherals.SUPC;
+
+	let mut scb = cortex_p.SCB;
+	let mut cpuid = cortex_p.CPUID;
+	scb.enable_icache();
+	scb.disable_dcache(&mut cpuid);
 
 	peripherals.EFC.eefc_fmr.write( |w| {
 		unsafe {w.fws().bits(5);}
@@ -54,13 +62,25 @@ fn main() -> ! {
 		&mut pmc
 	).unwrap();
 
+	let mut delay = Delay::new(cortex_p.SYST, &clocks);
+
+	//setup sdram and write and read some values for testing
 	let sdramc = peripherals.SDRAMC;
 	let sdram = init_sdram(&mut pmc, sdramc, &clocks);
 
-	let data : u32 = 42;
-	let addr = unsafe{ sdram.start_address().offset(1) } as *mut u32;
+	let data : u16 = 42 + 256;
+	let addr = unsafe{ sdram.start_address().offset(300) } as *mut u16;
 
+	writeln!(serial, "-----------------------------\r").unwrap();
 	writeln!(serial, "{:?}\r", addr).unwrap();
+	unsafe {core::ptr::write_volatile(addr, data)};
+	unsafe {core::ptr::write_volatile(addr.offset(1), data+512)};
+
+	delay.delay_ms(1 as u32);
+
+	writeln!(serial, "{:?}\r", unsafe{ *(addr.offset(1)) }).unwrap();
+	writeln!(serial, "{:?}\r", unsafe{ *((addr as *mut u8).offset(1) as *mut u16) }).unwrap();
+	writeln!(serial, "{:?}\r", unsafe{ *addr }).unwrap();
 
 	//enter infinite loop at end
 	loop {
--- src/mem.rs
+++ src/mem.rs
@@ -8,145 +8,112 @@ use atsamx7x_hal::gpio::*;
 use atsamx7x_hal::time::{NanoSeconds, PicoSeconds};
 use atsamx7x_hal::delay;
 
-pub struct NoCs;
-impl CS for NoCs {}
+pub struct SdramPins {
+		_a0 : pioc::PC20<PeripheralCntr<PeriphA>>,
+		_a1 : pioc::PC21<PeripheralCntr<PeriphA>>,
+		_a2 : pioc::PC22<PeripheralCntr<PeriphA>>,
+		_a3 : pioc::PC23<PeripheralCntr<PeriphA>>,
+		_a4 : pioc::PC24<PeripheralCntr<PeriphA>>,
+		_a5 : pioc::PC25<PeripheralCntr<PeriphA>>,
+		_a6 : pioc::PC26<PeripheralCntr<PeriphA>>,
+		_a7 : pioc::PC27<PeripheralCntr<PeriphA>>,
+		_a8 : pioc::PC28<PeripheralCntr<PeriphA>>,
+		_a9 : pioc::PC29<PeripheralCntr<PeriphA>>,
+		_a10 : piod::PD13<PeripheralCntr<PeriphC>>,
+		_a11 : pioc::PC31<PeripheralCntr<PeriphA>>,
+		_a12 : pioa::PA18<PeripheralCntr<PeriphC>>,
+
+		_d0 : pioc::PC0<PeripheralCntr<PeriphA>>,
+		_d1 : pioc::PC1<PeripheralCntr<PeriphA>>,
+		_d2 : pioc::PC2<PeripheralCntr<PeriphA>>,
+		_d3 : pioc::PC3<PeripheralCntr<PeriphA>>,
+		_d4 : pioc::PC4<PeripheralCntr<PeriphA>>,
+		_d5 : pioc::PC5<PeripheralCntr<PeriphA>>,
+		_d6 : pioc::PC6<PeripheralCntr<PeriphA>>,
+		_d7 : pioc::PC7<PeripheralCntr<PeriphA>>,
+		_d8 : pioe::PE0<PeripheralCntr<PeriphA>>,
+		_d9 : pioe::PE1<PeripheralCntr<PeriphA>>,
+		_d10 : pioe::PE2<PeripheralCntr<PeriphA>>,
+		_d11 : pioe::PE3<PeripheralCntr<PeriphA>>,
+		_d12 : pioe::PE4<PeripheralCntr<PeriphA>>,
+		_d13 : pioe::PE5<PeripheralCntr<PeriphA>>,
+		_d14 : pioa::PA15<PeripheralCntr<PeriphA>>,
+		_d15 : pioa::PA16<PeripheralCntr<PeriphA>>,
+
+		_b0 : pioa::PA20<PeripheralCntr<PeriphC>>,
+		_b1 : pioa::PA0<PeripheralCntr<PeriphC>>,
+
+		_msk0 : pioc::PC18<PeripheralCntr<PeriphA>>,
+		_msk1 : piod::PD15<PeripheralCntr<PeriphC>>,
+
+		_clk : piod::PD23<PeripheralCntr<PeriphC>>,
+		_cke : piod::PD14<PeripheralCntr<PeriphC>>,
+		_ras : piod::PD16<PeripheralCntr<PeriphC>>,
+		_cas : piod::PD17<PeripheralCntr<PeriphC>>,
+		_we : piod::PD29<PeripheralCntr<PeriphC>>
+}
+
+impl Default for SdramPins {
+	fn default() -> SdramPins {
+		let same = unsafe{Same_p::steal()};
+		let mut pmc = same.PMC;
+		//Set Pins to correct mode
+		let pioa = same.PIOA.split(&mut pmc);
+		let pioc = same.PIOC.split(&mut pmc);
+		let piod = same.PIOD.split(&mut pmc);
+		let pioe = same.PIOE.split(&mut pmc);
+
+		SdramPins {
+			_a0 : pioc.p20.into_pmd0(),
+			_a1 : pioc.p21.into_pmd0(),
+			_a2 : pioc.p22.into_pmd0(),
+			_a3 : pioc.p23.into_pmd0(),
+			_a4 : pioc.p24.into_pmd0(),
+			_a5 : pioc.p25.into_pmd0(),
+			_a6 : pioc.p26.into_pmd0(),
+			_a7 : pioc.p27.into_pmd0(),
+			_a8 : pioc.p28.into_pmd0(),
+			_a9 : pioc.p29.into_pmd0(),
+			_a10 : piod.p13.into_pmd2(),
+			_a11 : pioc.p31.into_pmd0(),
+			_a12 : pioa.p18.into_pmd2(),
+
+			_b0 : pioa.p20.into_pmd2(),
+			_b1 : pioa.p0.into_pmd2(),
+
+			_d0 : pioc.p0.into_pmd0(),
+			_d1 : pioc.p1.into_pmd0(),
+			_d2 : pioc.p2.into_pmd0(),
+			_d3 : pioc.p3.into_pmd0(),
+			_d4 : pioc.p4.into_pmd0(),
+			_d5 : pioc.p5.into_pmd0(),
+			_d6 : pioc.p6.into_pmd0(),
+			_d7 : pioc.p7.into_pmd0(),
+			_d8 : pioe.p0.into_pmd0(),
+			_d9 : pioe.p1.into_pmd0(),
+			_d10 : pioe.p2.into_pmd0(),
+			_d11 : pioe.p3.into_pmd0(),
+			_d12 : pioe.p4.into_pmd0(),
+			_d13 : pioe.p5.into_pmd0(),
+			_d14 : pioa.p15.into_pmd0(),
+			_d15 : pioa.p16.into_pmd0(),
+
+			_cke : piod.p14.into_pmd2(),
+			_clk : piod.p23.into_pmd2(),
+			_cas : piod.p17.into_pmd2(),
+			_ras : piod.p16.into_pmd2(),
+			_we : piod.p29.into_pmd2(),
+			_msk0 : pioc.p18.into_pmd0(),
+			_msk1 : piod.p15.into_pmd2()
+		}
+	}
+}
+
+impl atsamx7x_hal::sdram::SdramPins for SdramPins {}
 
 pub fn init_sdram(pmc: &mut PMC, sdramc: SDRAMC, clocks: &Clocks)
-->Sdram<(
-		pioc::PC20<PeripheralCntr<PeriphA>>,
-		pioc::PC21<PeripheralCntr<PeriphA>>,
-		pioc::PC22<PeripheralCntr<PeriphA>>,
-		pioc::PC23<PeripheralCntr<PeriphA>>,
-		pioc::PC24<PeripheralCntr<PeriphA>>,
-		pioc::PC25<PeripheralCntr<PeriphA>>,
-		pioc::PC26<PeripheralCntr<PeriphA>>,
-		pioc::PC27<PeripheralCntr<PeriphA>>,
-		pioc::PC28<PeripheralCntr<PeriphA>>,
-		pioc::PC29<PeripheralCntr<PeriphA>>,
-		piod::PD13<PeripheralCntr<PeriphC>>,
-		pioc::PC31<PeripheralCntr<PeriphA>>,
-		pioa::PA18<PeripheralCntr<PeriphC>>,
-		pioc::PC0<PeripheralCntr<PeriphA>>,
-		pioc::PC1<PeripheralCntr<PeriphA>>,
-		pioc::PC2<PeripheralCntr<PeriphA>>,
-		pioc::PC3<PeripheralCntr<PeriphA>>,
-		pioc::PC4<PeripheralCntr<PeriphA>>,
-		pioc::PC5<PeripheralCntr<PeriphA>>,
-		pioc::PC6<PeripheralCntr<PeriphA>>,
-		pioc::PC7<PeripheralCntr<PeriphA>>,
-		pioe::PE0<PeripheralCntr<PeriphA>>,
-		pioe::PE1<PeripheralCntr<PeriphA>>,
-		pioe::PE2<PeripheralCntr<PeriphA>>,
-		pioe::PE3<PeripheralCntr<PeriphA>>,
-		pioe::PE4<PeripheralCntr<PeriphA>>,
-		pioe::PE5<PeripheralCntr<PeriphA>>,
-		pioa::PA15<PeripheralCntr<PeriphA>>,
-		pioa::PA16<PeripheralCntr<PeriphA>>,
-		pioa::PA20<PeripheralCntr<PeriphC>>,
-		pioa::PA0<PeripheralCntr<PeriphC>>,
-		pioc::PC18<PeripheralCntr<PeriphA>>,
-		piod::PD15<PeripheralCntr<PeriphC>>,
-		piod::PD23<PeripheralCntr<PeriphC>>,
-		piod::PD14<PeripheralCntr<PeriphC>>,
-		NoCs,
-		piod::PD16<PeripheralCntr<PeriphC>>,
-		piod::PD17<PeripheralCntr<PeriphC>>,
-		piod::PD29<PeripheralCntr<PeriphC>>
-	)>
-	{
-	let same = unsafe{Same_p::steal()};
-	//Set Pins to correct mode
-	let pioa = same.PIOA.split(pmc);
-	let pioc = same.PIOC.split(pmc);
-	let piod = same.PIOD.split(pmc);
-	let pioe = same.PIOE.split(pmc);
-
-	let _sdram_addr0 = pioc.p20.into_pmd0();
-	let _sdram_addr1 = pioc.p21.into_pmd0();
-	let _sdram_addr2 = pioc.p22.into_pmd0();
-	let _sdram_addr3 = pioc.p23.into_pmd0();
-	let _sdram_addr4 = pioc.p24.into_pmd0();
-	let _sdram_addr5 = pioc.p25.into_pmd0();
-	let _sdram_addr6 = pioc.p26.into_pmd0();
-	let _sdram_addr7 = pioc.p27.into_pmd0();
-	let _sdram_addr8 = pioc.p28.into_pmd0();
-	let _sdram_addr9 = pioc.p29.into_pmd0();
-	let _sdram_addr10 = piod.p13.into_pmd2();
-	let _sdram_addr11 = pioc.p31.into_pmd0();
-	let _sdram_addr12 = pioa.p18.into_pmd2();
-
-	let _sdram_ba0 = pioa.p20.into_pmd2();
-	let _sdram_ba1 = pioa.p0.into_pmd2();
-
-	let _sdram_data0 = pioc.p0.into_pmd0();
-	let _sdram_data1 = pioc.p1.into_pmd0();
-	let _sdram_data2 = pioc.p2.into_pmd0();
-	let _sdram_data3 = pioc.p3.into_pmd0();
-	let _sdram_data4 = pioc.p4.into_pmd0();
-	let _sdram_data5 = pioc.p5.into_pmd0();
-	let _sdram_data6 = pioc.p6.into_pmd0();
-	let _sdram_data7 = pioc.p7.into_pmd0();
-	let _sdram_data8 = pioe.p0.into_pmd0();
-	let _sdram_data9 = pioe.p1.into_pmd0();
-	let _sdram_data10 = pioe.p2.into_pmd0();
-	let _sdram_data11 = pioe.p3.into_pmd0();
-	let _sdram_data12 = pioe.p4.into_pmd0();
-	let _sdram_data13 = pioe.p5.into_pmd0();
-	let _sdram_data14 = pioa.p15.into_pmd0();
-	let _sdram_data15 = pioa.p16.into_pmd0();
-
-	let _sdram_cke = piod.p14.into_pmd2();
-	let _sdram_clk = piod.p23.into_pmd2();
-	let _sdram_cas = piod.p17.into_pmd2();
-	let _sdram_ras = piod.p16.into_pmd2();
-	let _sdram_we = piod.p29.into_pmd2();
-	let _sdram_dqml = pioc.p18.into_pmd0();
-	let _sdram_dqmh = piod.p15.into_pmd2();
-
-	let _sdram_cs = NoCs{};
-
-	let pins = (
-		_sdram_addr0,
-		_sdram_addr1,
-		_sdram_addr2,
-		_sdram_addr3,
-		_sdram_addr4,
-		_sdram_addr5,
-		_sdram_addr6,
-		_sdram_addr7,
-		_sdram_addr8,
-		_sdram_addr9,
-		_sdram_addr10,
-		_sdram_addr11,
-		_sdram_addr12,
-		_sdram_data0,
-		_sdram_data1,
-		_sdram_data2,
-		_sdram_data3,
-		_sdram_data4,
-		_sdram_data5,
-		_sdram_data6,
-		_sdram_data7,
-		_sdram_data8,
-		_sdram_data9,
-		_sdram_data10,
-		_sdram_data11,
-		_sdram_data12,
-		_sdram_data13,
-		_sdram_data14,
-		_sdram_data15,
-		_sdram_ba0,
-		_sdram_ba1,
-		_sdram_dqml,
-		_sdram_dqmh,
-		_sdram_clk,
-		_sdram_cke,
-		_sdram_cs,
-		_sdram_ras,
-		_sdram_cas,
-		_sdram_we
-		);
-
+->Sdram<SdramPins>{
+	let pins = SdramPins::default();
 
 	let conf = SdramConfig {
 		banks : SdramBanks::Bank4,
